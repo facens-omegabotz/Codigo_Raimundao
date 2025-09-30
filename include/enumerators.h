739d1a61b282fc634410abb30049b03705c31527
MastroCoder <fe03mastromauro@gmail.com>
2025-09-30 08:43:43 -0300

    🚚move(include): Mudança de headers para a pasta default de inclusão do platform.io

---
+++ include/enumerators.h
@@ -0,0 +1,17 @@
+#ifndef HEADERS_ENUMERATORS_H_
+#define HEADERS_ENUMERATORS_H_
+#endif
+
+enum RobotState {
+  kReady = 0x0, // Comando HEX recebido pelo número 1.
+  kRunning,
+  kStop
+};
+
+enum Strategy {
+  kRadarEsq = 0x3, // Comando HEX recebido pelo número 4.
+  kRadarDir,
+  kCurvaAberta,
+  kFollowOnly,
+  kWoodPecker
+};
---
+++ include/sensing.h
@@ -0,0 +1,68 @@
+#ifndef HEADERS_SENSING_H_
+#define HEADERS_SENSING_H_
+#endif
+
+#include <IRremote.hpp>
+#include <QTRSensors.h>
+#include "enumerators.h"
+
+#define DECODE_SONY // Limita biblioteca de IR ao protocolo Sony.
+#define NUM_SENSORS 2
+#define NUM_SAMPLES_PER_SENSOR 4
+
+constexpr uint8_t kSensor1 = 35;
+constexpr uint8_t kSensor2 = 33;
+constexpr uint8_t kSensor3 = 25;
+constexpr uint8_t kSensor4 = 27;
+constexpr uint8_t kIrPin = 4;
+constexpr uint8_t kQtr1 = 36;
+constexpr uint8_t kQtr2 = 39;
+
+class SensingHandler {
+    private:
+        RobotState state = kReady;
+        Strategy strat = kRadarDir;
+        bool useNVSCalibration;
+        QTRSensorsAnalog qtra;
+    public:
+        SensingHandler(bool useNVSCalibration, bool aEnableLEDFeedback){
+            this->useNVSCalibration = useNVSCalibration;
+            qtra.init((uint8_t[]){kQtr1, kQtr2}, NUM_SENSORS, NUM_SAMPLES_PER_SENSOR);
+            IrReceiver.begin(kIrPin, false);
+        }
+
+        void receiveIrSignal(){
+            if (IrReceiver.decode()){
+                IrReceiver.resume();
+                if (state != kStop && state != kRunning && IrReceiver.decodedIRData.command == kReady)
+                    state = kReady;
+
+                if (state == kReady){
+                    switch(IrReceiver.decodedIRData.command){
+                        case kRunning:
+                        state = kRunning;
+                        break;
+                        case kRadarEsq:
+                        strat = kRadarEsq;
+                        break;
+                        case kRadarDir:
+                        strat = kRadarDir;
+                        break;
+                        case kCurvaAberta:
+                        strat = kCurvaAberta;
+                        break;
+                        case kFollowOnly:
+                        strat = kFollowOnly;
+                        break;
+                        case  kWoodPecker:
+                        strat = kWoodPecker;
+                        default:
+                        break;
+                    }
+                }
+                if (IrReceiver.decodedIRData.command == kStop)
+                    state = kStop;
+            }
+            IrReceiver.resume(); // might be unnecessary
+        }
+};
